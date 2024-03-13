f8e0b38e4204740abeb33d4241015fa9a0d55d55
robrechtdr <de.rouck.robrecht@gmail.com>
2024-03-13 12:26:29 +0100

    Test and implement variation 1

--- max_subarray.py
+++ max_subarray.py
@@ -34,3 +34,36 @@ def max_subarray(array):
             j += 1
           
     return max_subarray_sum
+
+
+def max_subarray_var1(array):
+    i = 0
+    j = 0 # var 1: j can be same pos as i
+    
+    # Can't set to 0; as updating mechanism wouldn't work properly if array
+    # would be only negative numbers. 
+    max_subarray_sum = None
+    max_subarray = None
+
+    while i <= len(array) - 1: # var 1: i can now be last pos
+        pass
+
+        subarray = array[i:j + 1]
+
+        subarray_sum = sum(subarray)
+        if not max_subarray:
+            max_subarray = subarray
+            max_subarray_sum = subarray_sum
+        
+        # Don't need to nest under else
+        if subarray_sum > max_subarray_sum:
+            max_subarray = subarray
+            max_subarray_sum = subarray_sum
+
+        if j == len(array) - 1:
+            i += 1
+            j = i # var 1: j can be same pos as i
+        else:
+            j += 1
+          
+    return max_subarray_sum
--- test_all.py
+++ test_all.py
@@ -1,5 +1,5 @@
 import pytest
-from max_subarray import max_subarray
+from max_subarray import max_subarray, max_subarray_var1
 
 
 def test_basic_example():
@@ -11,6 +11,18 @@ def test_basic_only_negatives():
     assert max_subarray(array) == -11
 
 def test_basic_smallest_array():
-    array = [2, 4]
-    assert max_subarray(array) == 6
+    array = [2, -1]
+    assert max_subarray(array) == 1
 
+
+def test_var1_example():
+    array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    assert max_subarray_var1(array) == 6
+
+def test_var1_only_negatives():
+    array = [-2, -9, -3]
+    assert max_subarray_var1(array) == -2
+
+def test_var1_smallest_array():
+    array = [2, -1]
+    assert max_subarray_var1(array) == 2
