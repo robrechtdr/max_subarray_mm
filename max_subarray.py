3257bd80832d97bc5473d9939db104947cf09eee
robrechtdr <de.rouck.robrecht@gmail.com>
2024-03-13 14:17:54 +0100

    Implement variation 2 with tests

--- max_subarray.py
+++ max_subarray.py
@@ -81,5 +81,98 @@ def max_subarray_var1(array: list[int | float]) -> int | float:
     return max_subarray_sum
 
 
-def max_subarray_var2(array: list[int | float]) -> int | float:
-    pass 
+def max_subarray_var2(array: list[int | float], bitmask: list[int]=None, bitmask_start_i: int=None) -> int | float:
+    """
+    Return the sum of numbers of the subarray with the largest sum. 
+
+    Takes a bitmask as additional argument
+
+    >>> max_subarray_var2(
+        array=[-2, 1, -3, 4, -1, 2, 1, -5, 4], 
+        bitmask=[1, 1, 0, 0],
+        bitmask_start_i=3
+    )
+    3
+
+    In the case above the bitmask is applied starting from index 3 of the array.
+    For every respective number with bitmask number 1: keep the number.
+    For every respective number with bitmask number 0: ignore the 
+    number (assume value 0). 
+    
+    So 4 in max subarray remains 4 because it has bitmask 1, 
+    2 in max subarray turns to 0 because it has bitmask 0.
+
+
+    This implementation assumes the purpose of the bitmask is only to change 
+    the value of the final sum number, not affecting the process to determine 
+    which subarray has the maximum sum. I.e. the sum returned will be based on
+    the same subarray regardless of which bitmask is applied.
+
+    Assumes the array min length is 1 and subarray min length is 1.
+    """
+    i = 0
+    j = 0 # var 1: j can be same pos as i
+    
+    # Can't set to 0; as updating mechanism wouldn't work properly if array
+    # would be only negative numbers. 
+    max_subarray_sum = None
+    max_subarray = None
+    max_subarray_i = None # var2
+
+    while i <= len(array) - 1: # var 1: i can now be last pos
+        subarray = array[i:j + 1]
+
+        subarray_sum = sum(subarray)
+        if not max_subarray:
+            max_subarray = subarray
+            max_subarray_sum = subarray_sum
+            max_subarray_i = i # var2
+        
+        # Don't need to nest under else
+        if subarray_sum > max_subarray_sum:
+            max_subarray = subarray
+            max_subarray_sum = subarray_sum
+            max_subarray_i = i # var2
+
+        if j == len(array) - 1:
+            i += 1
+            j = i # var 1: j can be same pos as i
+        else:
+            j += 1
+          
+    if bitmask: # var 2
+        max_subarray_len = len(max_subarray)
+
+        def apply_bitmask(array: list[float|int], bitmask: list[int], bitmask_start_i:int) -> list[int|float]:
+            """
+            >>> apply_bitmask([2, 10, 2, 3, 6], [0, 0, 1], 1)
+            [2, 0, 0, 3, 6]
+            """
+
+            # Get the version of the bitmask with same length as array
+            head = array[:bitmask_start_i]
+            tail = array[bitmask_start_i + len(bitmask):]
+            normalized_bitmask = [1] * len(head) + bitmask + [1] * len(tail)
+
+            result = []
+            for i, num in enumerate(array):
+                if normalized_bitmask[i] == 1:
+                    result.append(num)
+                elif normalized_bitmask[i] == 0:
+                    result.append(0)
+                else: 
+                    raise ValueError('Bitmask can only contain 0 or 1')
+
+            return result 
+        
+        bitmasked_array = apply_bitmask(
+            array=array, 
+            bitmask=bitmask, 
+            bitmask_start_i=bitmask_start_i
+        )
+
+        bitmasked_max_subarray = (bitmasked_array[max_subarray_i : max_subarray_i + len(max_subarray)])
+        print(bitmasked_max_subarray)
+        return sum(bitmasked_max_subarray)
+
+    return max_subarray_sum
--- test_all.py
+++ test_all.py
@@ -1,7 +1,8 @@
 import pytest
-from max_subarray import max_subarray, max_subarray_var1
+from max_subarray import max_subarray, max_subarray_var1, max_subarray_var2
 
 
+# Basic cases
 def test_basic_example():
     array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     assert max_subarray(array) == 6
@@ -14,7 +15,7 @@ def test_basic_smallest_array():
     array = [2, -1]
     assert max_subarray(array) == 1
 
-
+# Variation 1 cases
 def test_var1_example():
     array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     assert max_subarray_var1(array) == 6
@@ -26,3 +27,33 @@ def test_var1_only_negatives():
 def test_var1_smallest_array():
     array = [2, -1]
     assert max_subarray_var1(array) == 2
+
+# Variation 2 cases
+def test_var2_example():
+    array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    assert max_subarray_var2(
+        array=array,
+        bitmask=[1, 1, 0, 0],
+        bitmask_start_i=3
+    ) == 3
+
+def test_var2_bitmask_no_effect_on_underlying_max_array_selection():
+    array = [-2, -9, -3]
+    assert max_subarray_var2(
+        array=array,
+        bitmask=[0, 0],
+        bitmask_start_i=1
+    ) == -2
+
+def test_var2_no_bitmask_args():
+    array = [-2, -9, -3]
+    assert max_subarray_var2(array=array) == -2
+
+
+def test_var2_bitmask_larger_than_array():
+    array = [-2, -9, -3]
+    assert max_subarray_var2(
+        array=array,
+        bitmask=[0, 0, 0, 0],
+        bitmask_start_i=1
+    ) == -2
